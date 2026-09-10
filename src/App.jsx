d447c6f69bea55069cbd0ecf3ae502420ed1ca75
TariqTechie-dev <tariqtech843@gmail.com>
2026-09-10 07:09:37 +0500

    Create main Page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
+import Page from "./page";
+
 function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-blue-600">
-      <h1 className="text-4xl font-bold text-white drop-shadow-md">
-        Tariq Hussain Portfolio — Tailwind Active! 🔥
-      </h1>
-    </div>
-  );
+  return <Page />;
 }
 
 export default App;
